94d762bb6d8553ba291b01b6eace0a3a4e2f7a78
vtamprateep <vtamprateep@gmail.com>
2024-04-05 12:03:39 -0400

    created first real route

--- server/routes.js
+++ server/routes.js
@@ -2,7 +2,7 @@ const postgres = require('postgres');
 const config = require('./config.json');
 
 
-const connection = postgres({
+const psql = postgres({
     host: config.host,
     username: config.user,
     password: config.pass,
@@ -15,6 +15,31 @@ const test = function (req, res) {
     res.send('response from express backend received!');
 }
 
+/* Route to get daily charts data. Would be cool to increase / decrease granularity by week / month */
+const get_charts = async function (req, res) {
+    body_params = req.body;
+    path_params = req.params;
+    psql`
+        SELECT
+            close_date,
+            open,
+            high,
+            low,
+            close,
+            adj_close,
+            volume,
+            unadjusted_volume,
+            change,
+            change_over_time,
+            vwap
+        FROM charts
+        WHERE symbol_id = ${path_params.symbol_id}
+            AND close_date BETWEEN ${path_params.start_date} AND ${path_params.end_date}
+        ;
+    `.then((result) => res.send(result));
+}
+
 module.exports = {
-    test
+    test,
+    get_charts
 }
