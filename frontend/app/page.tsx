2a4d843da8b390981da6477280a3bb0aece7220d
vtamprateep <vtamprateep@gmail.com>
2024-04-15 11:40:18 -0400

    dynamic render profile data, add type

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -7,38 +7,22 @@ import config from "../next.config.mjs";
 
 
 function SymbolProfile() {
-  const [profileData, setProfileData] = useState({
-    symbol:"IWM",
-    company_name:"iShares Russell 2000 ETF",
-    currency:"USD",
-    cik:"1100663",
-    isin:"US4642876555",
-    exchange:"New York Stock Exchange Arca",
-    cusip:"464287655",
-    exchange_short_name:"AMEX",
-    industry:null,
-    website:"https://www.ishares.com/us/products/239710/ishares-russell-2000-etf",
-    description:"The fund generally invests at least 80% of its assets in the component securities of its underlying index and in investments that have economic characteristics that are substantially identical to the component securities of its underlying index (i.e., depositary receipts representing securities of the underlying index) and may invest up to 20% of its assets in certain futures, options and swap contracts, cash and cash equivalents.",
-    ceo:null,
-    sector:"Real Estate",
-    country:"US",
-    phone:null,
-    address:"400 Howard Street",
-    city:"San Francisco",
-    state:null,
-    ipo_date:"2000-05-26T04:00:00.000Z",
-    is_etf:true,
-    is_actively_trading:true,
-    is_adr:false,
-    is_fund:false
-  });
+  const [profileData, setProfileData] = useState<ProfileData>();
 
-  return <table>{Object.entries(profileData).map(([key, value]) => (
-    <tr key={key}>
-      <td>{key}</td>
-      <td>{value}</td>
-    </tr>
-  ))}</table>;
+  useEffect(() => {
+    fetch(`http://${config.env.SERVER_HOST}:${config.env.SERVER_PORT}/get_profile/10`, {
+      method: "GET",
+      headers: { 'Content-Type': 'application/json' },
+    })
+    .then((res) => res.json())
+    .then((res_data: any) => {
+      setProfileData(res_data.rows[0]);
+    })
+  }, []);
+
+  return (<table>{
+    profileData ? Object.entries(profileData).map(([key, value]) => (<tr key={key}><td>{key}</td><td>{String(value)}</td></tr>)) : null
+  }</table>);
 }
 
 export default function Home() {
@@ -55,7 +39,7 @@ export default function Home() {
     })
     .then((res) => res.json())
     .then((res_data: any) => {
-      let newChartData = {
+      setChartData({
         labels: res_data.rows.map((row: any) => row.close_date.slice(0, 10)),
         datasets: [
           {
@@ -63,10 +47,9 @@ export default function Home() {
             data: res_data.rows.map((row: any) => row.adj_close)
           }
         ]
-      }
-      setChartData(newChartData);
+      });
     }).catch(error => {console.log("Error fetching or processing data:", error)});
-  });
+  }, []);
 
   return (
     <div>
--- frontend/types.d.ts
+++ frontend/types.d.ts
@@ -5,4 +5,29 @@ declare global {
         labels: string[],
         datasets: {label: string, data: number[]}[]
     }
+    interface ProfileData {
+        symbol: string,
+        company_name: string,
+        currency: string,
+        cik: string,
+        isin: string,
+        exchange: string,
+        cusip: string,
+        exchange_short_name: string,
+        industry: string,
+        website: string,
+        description: string,
+        ceo: string,
+        sector: string,
+        country: string,
+        phone: string,
+        address: string,
+        city: string,
+        state: string,
+        ipo_date: string,
+        is_etf: boolean,
+        is_actively_trading: boolean,
+        is_adr: boolean,
+        is_fund: boolean
+      }
 }
