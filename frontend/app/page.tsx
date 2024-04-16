31c26d8660aa2fd7e980389681be38e158ba8f89
vtamprateep <vtamprateep@gmail.com>
2024-04-16 17:01:52 -0400

    move components to module

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,59 +1,12 @@
 'use client'
 
-import { Chart } from 'react-chartjs-2';
-import 'chart.js/auto';
-import { useEffect, useState } from 'react';
-import config from "../next.config.mjs";
-
-
-function SymbolProfile() {
-  const [profileData, setProfileData] = useState<ProfileData>();
-
-  useEffect(() => {
-    fetch(`http://${config.env.SERVER_HOST}:${config.env.SERVER_PORT}/get_profile/10`, {
-      method: "GET",
-      headers: { 'Content-Type': 'application/json' },
-    })
-    .then((res) => res.json())
-    .then((res_data: any) => {
-      setProfileData(res_data.rows[0]);
-    })
-  }, []);
-
-  return (<table>{
-    profileData ? Object.entries(profileData).map(([key, value]) => (<tr key={key}><td>{key}</td><td>{String(value)}</td></tr>)) : null
-  }</table>);
-}
+import { SymbolProfile, PriceChart } from './components';
 
 export default function Home() {
 
-  const [chartData, setChartData] = useState<ChartData>({
-    labels: ["1", "2", "3"],
-    datasets: [{label: "X", data: [1, 2, 3]}]
-  });
-
-  useEffect(() => {
-    fetch(`http://${config.env.SERVER_HOST}:${config.env.SERVER_PORT}/get_charts/10`, {
-      method: "GET",
-      headers: { 'Content-Type': 'application/json' },
-    })
-    .then((res) => res.json())
-    .then((res_data: any) => {
-      setChartData({
-        labels: res_data.rows.map((row: any) => row.close_date.slice(0, 10)),
-        datasets: [
-          {
-            label: "Adjusted Close Price",
-            data: res_data.rows.map((row: any) => row.adj_close)
-          }
-        ]
-      });
-    }).catch(error => {console.log("Error fetching or processing data:", error)});
-  }, []);
-
   return (
     <div>
-      <Chart type="line" data={chartData} />
-      <SymbolProfile />
+      <PriceChart symbolId={10}/>
+      <SymbolProfile symbolId={10}/>
     </div>);
 }
