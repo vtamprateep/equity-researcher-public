f9b328c5c4848da5ee82ef14d07fe6ed924cacdc
vtamprateep <vtamprateep@gmail.com>
2024-04-17 16:27:45 -0400

    add search functionality

--- frontend/app/components.tsx
+++ frontend/app/components.tsx
@@ -4,46 +4,57 @@ import { useEffect, useState } from 'react';
 import config from "../next.config.mjs";
 
 
-export function SymbolProfile({symbolId}: {symbolId: number}) {
+export function SymbolProfile({symbolId}: {symbolId: number | undefined}) {
     const [profileData, setProfileData] = useState<ProfileData>();
-    const [curSymbolId, setCurSymbolId] = useState<number>(symbolId);
+    // const [curSymbolId, setCurSymbolId] = useState<number | undefined>(symbolId);
   
     useEffect(() => {
-        fetch(`http://${config?.env?.SERVER_HOST}:${config?.env?.SERVER_PORT}/get_profile/${curSymbolId}`, {
-            method: "GET",
-            headers: { 'Content-Type': 'application/json' },
-        })
-        .then((res) => res.json())
-        .then((res_data: any) => {
-            setProfileData(res_data.rows[0]);
-        })
-    }, []);
+        console.log("SymbolProfile updated.");
+        if (symbolId != undefined) {
+            fetch(`http://${config?.env?.SERVER_HOST}:${config?.env?.SERVER_PORT}/get_profile/${symbolId}`, {
+                method: "GET",
+                headers: { 'Content-Type': 'application/json' },
+            })
+            .then((res) => res.json())
+            .then((res_data: any) => {
+                setProfileData(res_data.rows[0]);
+            })
+            
+        }
+        
+    }, [symbolId]);
   
-    return (<table>{
-        profileData ? Object.entries(profileData).map(([key, value]) => (<tr key={key}><td>{key}</td><td>{String(value)}</td></tr>)) : null
-    }</table>);
+    if (symbolId != undefined) {
+        return (<table>{
+            profileData ? Object.entries(profileData).map(([key, value]) => (<tr key={key}><td>{key}</td><td>{String(value)}</td></tr>)) : null
+        }</table>);
+    }
 }
 
-export function PriceChart({symbolId}: {symbolId: number}) {
+export function PriceChart({symbolId}: {symbolId: number | undefined}) {
     const [chartData, setChartData] = useState<ChartData>({ labels: [], datasets: [] });
-    const [curSymbolId, setCurSymbolId] = useState<number>(symbolId);
+    // const [curSymbolId, setCurSymbolId] = useState<number | undefined>(symbolId);
     
     useEffect(() => {
-        fetch(`http://${config?.env?.SERVER_HOST}:${config?.env?.SERVER_PORT}/get_charts/${curSymbolId}`, {
-            method: "GET",
-            headers: { 'Content-Type': 'application/json' },
-        })
-        .then((res) => res.json())
-        .then((res_data: any) => {
-            setChartData({
-                labels: res_data.rows.map((row: any) => row.close_date.slice(0, 10)),
-                datasets: [{
-                    label: "Adjusted Close Price",
-                    data: res_data.rows.map((row: any) => row.adj_close)
-                }]
-            });
-        }).catch(error => {console.log("Error fetching or processing data:", error)});
-    }, []);
+        if (symbolId != undefined) {
+            fetch(`http://${config?.env?.SERVER_HOST}:${config?.env?.SERVER_PORT}/get_charts/${symbolId}`, {
+                method: "GET",
+                headers: { 'Content-Type': 'application/json' },
+            })
+            .then((res) => res.json())
+            .then((res_data: any) => {
+                setChartData({
+                    labels: res_data.rows.map((row: any) => row.close_date.slice(0, 10)),
+                    datasets: [{
+                        label: "Adjusted Close Price",
+                        data: res_data.rows.map((row: any) => row.adj_close)
+                    }]
+                });
+            }).catch(error => {console.log("Error fetching or processing data:", error)});
+        }
+    }, [symbolId]);
     
-    return <Chart type="line" data={chartData} />;
+    if (symbolId != undefined) {
+        return <Chart type="line" data={chartData} />;
+    }
 }
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,44 @@
 'use client'
 
 import { SymbolProfile, PriceChart } from './components';
+import { useEffect, useState } from 'react';
+import config from "../next.config.mjs";
+
 
 export default function Home() {
 
+  const [input, setInput] = useState<string>('');
+  const [focusSymbolId, setFocusSymbolId] = useState<number>();
+
+  const updateInput = (e: any) => {
+    setInput(e.target.value);
+  }
+
+  const submitInputHandler = (e: any) => {
+    if (e.key == "Enter" && input != undefined) {
+      console.log(`http://${config?.env?.SERVER_HOST}:${config?.env?.SERVER_PORT}/get_symbolid/${input.toUpperCase()}`);
+      fetch(`http://${config?.env?.SERVER_HOST}:${config?.env?.SERVER_PORT}/get_symbolid/${input.toUpperCase()}`)
+      .then(res => res.json())
+      .then((res_data: any) => {
+        if (res_data.rows.length != 0) {
+          setFocusSymbolId(res_data.rows[0].id);
+          console.log("Set focused symbol ID to:", focusSymbolId);
+        }
+      })
+    }
+  }
+
+  useEffect(() => {
+    if (focusSymbolId !== undefined) {
+      // Fetch data or perform any action based on the updated focusSymbolId
+      console.log("Fetching data for symbol ID:", focusSymbolId);
+    }
+  }, [focusSymbolId]);
+
   return (
     <div>
-      <PriceChart symbolId={10}/>
-      <SymbolProfile symbolId={10}/>
+      <input type="text" id="searchInputField" value={input} onChange={updateInput} onKeyDown={submitInputHandler} />
+      <PriceChart symbolId={focusSymbolId}/>
+      <SymbolProfile symbolId={focusSymbolId}/>
     </div>);
 }
