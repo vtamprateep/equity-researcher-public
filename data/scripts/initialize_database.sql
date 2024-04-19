a5b6719ac286830d1e7126f6cc850c3f5190d8d1
vtamprateep <vtamprateep@gmail.com>
2024-04-19 16:44:55 -0400

    more seed tables and scripts

--- data/scripts/initialize_database.sql
+++ data/scripts/initialize_database.sql
@@ -1,29 +1,8 @@
-/* Create company_profile table if doesn't exist */
+/* Create symbols table if doesn't exist */
+DROP TABLE IF EXISTS symbol;
 CREATE TABLE symbol (
     id SERIAL PRIMARY KEY,
-    symbol VARCHAR(20),
-    company_name VARCHAR(100),
-    currency VARCHAR(20),
-    cik VARCHAR(100),
-    isin VARCHAR(100),
-    cusip VARCHAR(100),
-    exchange VARCHAR(100),
-    exchange_short_name VARCHAR(100),
-    industry VARCHAR(100),
-    website VARCHAR(200),
-    "description" TEXT,
-    ceo VARCHAR(100),
-    sector VARCHAR(100),
-    country VARCHAR(100),
-    phone VARCHAR(100),
-    "address" VARCHAR(100),
-    city VARCHAR(100),
-    "state" VARCHAR(100),
-    ipo_date DATE,
-    is_etf BOOLEAN,
-    is_actively_trading BOOLEAN,
-    is_adr BOOLEAN,
-    is_fund BOOLEAN
+    symbol VARCHAR(20)
 );
 
 /* Create charts table if doesn't exist */
@@ -36,18 +15,11 @@ CREATE TABLE charts (
     high DOUBLE PRECISION,
     low DOUBLE PRECISION,
     "close" DOUBLE PRECISION,
-    adj_close DOUBLE PRECISION,
-    volume INTEGER,
-    unadjusted_volume INTEGER,
-    change DOUBLE PRECISION,
-    change_percent DOUBLE PRECISION,
-    vwap REAL,
-    label DATE,
-    change_over_time DOUBLE PRECISION
+    adj_close DOUBLE PRECISION
 );
 
 /* Load data into symbol */
 COPY symbol FROM '/Users/vtamprateep/Documents/GitHub/value-investing-research-platform/data_scripts/symbol.csv' DELIMITER ',' CSV HEADER;
 
 /* Load data into charts */
-COPY charts FROM '/Users/vtamprateep/Documents/GitHub/value-investing-research-platform/data/db_tables/charts/charts.csv' DELIMITER ',' CSV HEADER;
+COPY charts FROM '/Users/vtamprateep/Documents/GitHub/value-investing-research-platform/data//charts/charts.csv' DELIMITER ',' CSV HEADER;
