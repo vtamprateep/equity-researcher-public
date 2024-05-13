4f196e70475c6365246af32e59b24677f27ac866
vtamprateep <vtamprateep@gmail.com>
2024-05-13 11:52:27 -0400

    add button search

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
         setInput(e.target.value);
     }
 
-    const submitInputHandler = (e: any) => {
-        if (e.key == "Enter" && input != undefined) {
+    const submitInputHandler = () => {
+        if (input != undefined && input.trim() !== "") {
             ServerRoutes.getSymbolId(input.toUpperCase())
                 .then(data => data.rows.length ? setFocusSymbolId(data.rows[0].id) : undefined)
                 .catch(error => { console.log(error) });
@@ -26,15 +26,22 @@ export default function Home() {
         <div>
             <div className="bg-slate-700 py-4 px-8 text-white flex items-center justify-between">
                 <h1 className="text-2xl font-bold">Equity Researcher</h1>
-                <input
-                    type="text"
-                    id="searchInputField"
-                    value={input}
-                    onChange={updateInput}
-                    onKeyDown={submitInputHandler}
-                    placeholder="Enter ticker symbol here..."
-                    className="px-4 py-2 rounded-md text-black"
-                />
+                <div>
+                    <input
+                        type="text"
+                        id="searchInputField"
+                        value={input}
+                        onChange={updateInput}
+                        onKeyDown={(e) => {if (e.key === "Enter") { submitInputHandler() }}}
+                        placeholder="Enter ticker symbol here..."
+                        className="px-4 py-2 rounded-md text-black"
+                    />
+                    <button 
+                        className="mx-4 rounded-md bg-zinc-600 px-4 py-2 border-black border"
+                        onClick={submitInputHandler}>
+                        Search
+                    </button>
+                </div>
             </div>
             {focusSymbolId &&
                 <div className="flex flex-col items-center h-screen mx-8 my-8">
